b1cff3ca657fe280946a892c6c14ac17ca71298a
RashmiKhanagwal <rashmi.khanagwal@gmail.com>
2022-05-05 16:17:16 +0530

    crud api controllers file

--- crud-app/controllers/articles.js
+++ crud-app/controllers/articles.js
@@ -4,7 +4,7 @@ exports.create = async(req,res)=>{
     const newArticle = new article(req.body);
     try{
        const savedArticle = await newArticle.save();
-       res.status(200).json(savedArticle);
+       res.status(200).json("Article has been created");
     }  catch (err){
         res.status(500).json(err);
     }
@@ -21,11 +21,25 @@ exports.getAll = async(req,res)=>{
 };
 
 exports.getOne = async(req,res)=> {
-    const Article = await article.findById(req.params.id);
+    try{
+        const Article = await article.findById(req.params.id);
+        res.status(200).json(Article);
+    } catch (err){
+        res.status(500).json(err);
+    }
+    
 };
 
-exports.update =async(req,res)=> {
-
+exports.update = async(req,res)=> {
+    try{
+        const updatedArticle = await article.findByIdAndUpdate(
+            req.params.id,
+            {$set: req.body},{new:true}
+        );
+        res.status(200).json(updatedArticle)
+    } catch (err){
+        res.status(500).json(err);
+    }
 };
 
 exports.delete = async(req,res)=>{
